6d3398c377772c01f7c033eab3b1e94ecc3b1d92
Khang <21130394@st.hcmuaf.edu.vn>
2024-08-03 14:54:31 +0700

    update

---
+++ Applicaion/Models/FilmStatus/FilmStatusResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Applicaion.Models.FilmStatus
+{
+    public record FilmStatusResponse(string FilmStatus)
+    {
+    }
+}
---
+++ Applicaion/Models/VoteRequest/VoteRequestResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Applicaion.Models.VoteRequest
+{
+    public record VoteRequestResponse(int Vote)
+    {
+    }
+}
