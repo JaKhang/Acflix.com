823c5e0a2433bd9d388463b47a4bcf75375d8238
Khang <21130394@st.hcmuaf.edu.vn>
2024-08-03 15:05:58 +0700

    update

---
+++ Applicaion/Models/Error/ErrorResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Applicaion.Models.Error
+{
+    public record ErrorResponse
+        (
+        int Status,
+        string Message
+        )
+    {
+    }
+}
